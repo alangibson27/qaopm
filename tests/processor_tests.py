23f9db8ed129d473886be5b7290a99db616ae75d
Alan Gibson <alangibson27@gmail.com>
2015-10-01 00:24:31 +0100

    Added ld a, (hl), ld a, (bc), ld a, (de), ld a, i

--- tests/processor_tests.py
+++ tests/processor_tests.py
@@ -29,11 +29,44 @@ class TestProcessor:
             yield self.check_single_cycle_register_load, op_code, destination, source
 
     def check_single_cycle_register_load(self, op_code, destination, source):
-            self.processor.main_registers[source] = 0xff
-            self.memory.poke(0x0, op_code)
+        self.processor.main_registers[source] = 0xff
+        self.memory.poke(0x0, op_code)
 
-            self.processor.single_cycle()
+        self.processor.single_cycle()
 
-            assert_equals(0x0001, self.processor.special_registers['pc'])
-            assert_equals(0xff, self.processor.main_registers[destination])
-            assert_equals(0xff, self.processor.main_registers[source])
+        assert_equals(0x0001, self.processor.special_registers['pc'])
+        assert_equals(0xff, self.processor.main_registers[destination])
+        assert_equals(0xff, self.processor.main_registers[source])
+
+    def test_single_cycle_register_loads_from_memory(self):
+        operations = [
+            (0x7e, 'a', 'hl'),
+            (0x0a, 'a', 'bc'),
+            (0x1a, 'a', 'de')
+        ]
+
+        for (op_code, destination, source_register) in operations:
+            yield self.check_single_cycle_register_load_from_memory, op_code, destination, source_register
+
+    def check_single_cycle_register_load_from_memory(self, op_code, destination, source_register):
+        self.processor.main_registers[source_register[0]] = 0xa0
+        self.processor.main_registers[source_register[1]] = 0xa0
+
+        self.memory.poke(0x0, op_code)
+        self.memory.poke(0xa0a0, 0xaa)
+
+        self.processor.single_cycle()
+
+        assert_equals(0x0001, self.processor.special_registers['pc'])
+        assert_equals(0xaa, self.processor.main_registers[destination])
+
+    def test_ld_a_i(self):
+        self.processor.special_registers['i'] = 0xff
+
+        self.memory.poke(0x0, 0xed)
+        self.memory.poke(0x1, 0x57)
+
+        self.processor.single_cycle()
+
+        assert_equals(0x0002, self.processor.special_registers['pc'])
+        assert_equals(0xff, self.processor.main_registers['a'])
--- z80/processor.py
+++ z80/processor.py
@@ -26,6 +26,10 @@ class Processor:
             0x7b: Op(lambda: self.register_load('a', 'e'), 'ld a, e'),
             0x7c: Op(lambda: self.register_load('a', 'f'), 'ld a, f'),
             0x7d: Op(lambda: self.register_load('a', 'l'), 'ld a, l'),
+            0x7e: Op(lambda: self.register_load_memory('a', 'hl'), 'ld a, (hl)'),
+            0x0a: Op(lambda: self.register_load_memory('a', 'bc'), 'ld a, (bc)'),
+            0x1a: Op(lambda: self.register_load_memory('a', 'de'), 'ld a, (de)'),
+
             0x4f: Op(lambda: self.register_load('c', 'a'), 'ld c, a'),
             0x48: Op(lambda: self.register_load('c', 'b'), 'ld c, b'),
             0x49: Op(lambda: self.register_load('c', 'c'), 'ld c, c'),
@@ -33,6 +37,13 @@ class Processor:
             0x4b: Op(lambda: self.register_load('c', 'e'), 'ld c, e'),
             0x4c: Op(lambda: self.register_load('c', 'f'), 'ld c, f'),
             0x4d: Op(lambda: self.register_load('c', 'l'), 'ld c, l'),
+
+            0xed: self.init_ed_opcodes()
+        }
+
+    def init_ed_opcodes(self):
+        return {
+            0x57: Op(self.ld_a_i, 'ld a, i')
         }
 
     def single_cycle(self):
@@ -40,17 +51,33 @@ class Processor:
         operation.function()
 
     def get_operation(self):
-        op_code = self.memory.peek(self.special_registers['pc'])
-        self.increment('pc')
-
+        op_code = self.get_op_code()
         operation = self.operations_by_opcode[op_code]
-        if isinstance(operation, list):
-            pass
+        if isinstance(operation, dict):
+            op_code = self.get_op_code()
+            operation = operation[op_code]
 
         return operation
 
+    def get_op_code(self):
+        op_code = self.memory.peek(self.special_registers['pc'])
+        self.increment('pc')
+        return op_code
+
     def increment(self, register_name):
-        self.special_registers[register_name] = self.special_registers[register_name] + 1
+        self.special_registers[register_name] += 1
 
     def register_load(self, destination, source):
         self.main_registers[destination] = self.main_registers[source]
+
+    def register_load_memory(self, destination, source_register):
+        msb = self.main_registers[source_register[0]]
+        lsb = self.main_registers[source_register[1]]
+        address = self.join(msb, lsb)
+        self.main_registers[destination] = self.memory.peek(address)
+
+    def ld_a_i(self):
+        self.main_registers['a'] = self.special_registers['i']
+
+    def join(self, msb, lsb):
+        return (msb << 8) + lsb

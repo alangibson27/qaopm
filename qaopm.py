e278c24cb618cf52c3d50725a0a6e7a2a6ff810f
Alan Gibson <alangibson27@gmail.com>
2016-01-24 11:18:17 +0000

    Launcher script

---
+++ qaopm.py
@@ -0,0 +1,10 @@
+import argparse
+from spectrum.computer import start
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rom_file', help='Spectrum ROM file')
+    parser.add_argument('snapshot_file', help='.SNA-format snapshot file')
+    args = parser.parse_args()
+
+    computer = start(args.rom_file, args.snapshot_file)
